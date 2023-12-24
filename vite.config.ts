1473990c48992368384615d02b1c33d5802e6f12
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-12-23 21:20:24 -0500

    [add][fix] Add PWA support to the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,37 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Spectra Steganography Suite',
+        short_name: 'Spectra',
+        description: 'Spectra Steganography Suite',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'vite.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'vite.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+          },
+        ],
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff'
+      },
+    }),
+  ],
   server: {
     port: 4002
-  }
+  },
 });
