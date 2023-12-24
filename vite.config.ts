964103c2321de4ad75d313111127ccada34789e4
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-12-24 12:00:58 -0500

    [add][fix] Update favicon and app icons

--- vite.config.ts
+++ vite.config.ts
@@ -15,12 +15,12 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'vite.svg',
+            src: 'spectra-icon.svg',
             sizes: '192x192',
             type: 'image/svg+xml',
           },
           {
-            src: 'vite.svg',
+            src: 'spectra-icon.svg',
             sizes: '512x512',
             type: 'image/svg+xml',
           },
