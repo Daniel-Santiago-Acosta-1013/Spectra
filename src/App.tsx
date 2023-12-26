ad2611ad3e8281fff6e34bb2bb53931c71d7acde
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-12-26 08:38:16 -0500

    [add][fix] Add fallback route to redirect to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import EncryptView from './views/EncryptView/EncryptView';
 import DecryptView from './views/DecryptView/DecryptView';
 
@@ -8,6 +8,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<EncryptView />} />
                 <Route path="/decrypt" element={<DecryptView />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
         </Router>
     );
